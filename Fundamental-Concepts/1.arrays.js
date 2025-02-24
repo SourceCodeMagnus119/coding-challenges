1a14861c88b5ae1abab536778f3ddf30613385ef
JH Paul <paulusg131@gmail.com>
2025-02-24 23:34:06 +0200

    Problem 4 + Added time complexity to all handler functions

--- Fundamental-Concepts/1.arrays.js
+++ Fundamental-Concepts/1.arrays.js
@@ -20,7 +20,7 @@ function findMax(arr) {
 
 // Test the function
 const numbers = [1, 3, 7, 2, 5];
-console.log(findMax(numbers)); // Output: 7
+console.log(findMax(numbers)); // Output: 7, Time Complexity: O(n).
 
 // Challenge: Find the maximum number in the array.
 
@@ -40,12 +40,12 @@ const max = (arr) => {
     }
     return max;
 }
-console.log(max(integers)); // Output: 22
+console.log(max(integers)); // Output: 22, Time Complexity: O(n).
 
 // Challenge: Find the maximum number in the array, starting from the last element.
 
 // Write a function called `reverseMax` that takes an array of numbers as an argument
-// ad returns the maximum number in the array starting from the last element.
+// and returns the maximum number in the array starting from the last element.
 
 const reverseMax = (arr) => {
     if(arr.length === 0) return null;
@@ -60,4 +60,24 @@ const reverseMax = (arr) => {
 };
 
 const inverseInt = [3, 38, 16, 21, 8, 56, 92];
-console.log(reverseMax(inverseInt));
+console.log(reverseMax(inverseInt)); // Output: 92, Time Complexity: O(n).
+
+// Challenge: Find the maximum number in the array.
+
+// Write a function called "bigInt" that takes an long array of numbers as an argument.
+// and returns the maximum number in the array.
+
+const nums = [7, 4, 39, 16, 18, 22, 23, 15, 25, 36, 19, 112, 92, 873, 228, 592, 925, 48, 82, 61, 119, 429, 123];
+
+const bigInt = (arr) => {
+    if(arr.length === 0) return null;
+    const max = arr[0];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+};
+
+console.log(bigInt(nums)); // Output: 925, Time Complexity: O(n).
