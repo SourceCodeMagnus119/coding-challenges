e14ffc40d41aa6001f07da48b15b196c152e76a8
JH Paul <paulusg131@gmail.com>
2025-05-04 22:51:27 +0200

    Problem 2: Implement a basic singly linked list. b2le4e5d8

--- Fundamental-Concepts/2.linkedList.js
+++ Fundamental-Concepts/2.linkedList.js
@@ -45,3 +45,52 @@ list.append(150);
 list.append(50);
 list.append(65);
 list.printList(); // Expected output: 10 -> 20 -> 30
+
+// Linked List Challenge: Implement a basic singly linked list
+
+// Create duall classes called 'Node' and 'LinkedList' that adds
+// and prints nodes.
+class N {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class L {
+	constructor() {
+		this.head = null;
+		this.size = 0;
+	}
+
+	append(value) {
+		const newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+		} else {
+			let current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+		this.size++;
+	}
+
+	printList() {
+		let current = this.head;
+		const elements = [];
+		while (current) {
+			elements.push(current.value);
+			current = current.next;
+		}
+		console.log(elements.join(" -> "));
+	}
+}
+
+const tester = new N();
+tester.append(900);
+tester.append(23);
+tester.append(49);
+tester.append(5);
+tester.printList(); // Expected Output: [], Time Complexity: 0(n).
